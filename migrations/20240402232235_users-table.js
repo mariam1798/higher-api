6e86918c55fb9b2afba1daceea940a6788583b79
mariam1798 <Mariam.alneamah@gmail.com>
2024-04-03 00:34:59 +0100

    create users table

---
+++ migrations/20240402232235_users-table.js
@@ -0,0 +1,19 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("location").notNullable();
+    table.string("professional_status").notNullable();
+    table.integer("experience_years").notNullable();
+    table.string("job_title").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("inventories");
+};
