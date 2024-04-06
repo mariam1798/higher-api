35b9a3ac175aef8adf38983c8554fb5733a859cd
mariam1798 <Mariam.alneamah@gmail.com>
2024-04-06 20:00:47 +0100

    create videos db

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
 require("dotenv").config({ path: "../.env" });
-
+DB_NAME = "higher";
+DB_USER = "root";
+DB_PASSWORD = "rootroot";
+DB_HOST = "127.0.0.1";
 module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      host: DB_HOST,
+      database: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
       charset: "utf8",
     },
   },
---
+++ migrations/20240406182000_create-videos-table.js
@@ -0,0 +1,29 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("videos", (table) => {
+    table.increments("id").primary();
+    table.string("title").notNullable();
+    table.string("channel");
+    table.string("image");
+    table.text("description");
+    table.string("views");
+    table.string("likes");
+    table.string("duration");
+    table.string("url");
+    table.bigInteger("timestamp");
+    table
+      .integer("user_id")
+      .unsigned()
+      .references("users.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("videos");
+};
